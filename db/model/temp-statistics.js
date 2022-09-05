b001944788dedbb4f5bdf009908ff22965328047
Urko <urkobein@riseup.net>
2022-09-05 11:34:53 +0200

    feat: add _id to statistics

--- db/model/temp-statistics.js
+++ db/model/temp-statistics.js
@@ -15,7 +15,7 @@ const statisticsSchema = new mongoose.Schema(
     day_volume: Number,
   },
   {
-    _id: false,
+    _id: true,
     timestamps: {
       createdAt: "created_at", // Use `created_at` to store the created date
       updatedAt: "updated_at", // and `updated_at` to store the last updated date
